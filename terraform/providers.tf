511481b2a4fd663281d7c8539d95702a92fd51ce
Jibin George <jibingeorge1964@gmail.com>
2025-12-19 21:12:43 +0530

    feat: Add Proxmox provider timeout configuration.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -12,4 +12,5 @@ provider "proxmox" {
   pm_api_token_id     = var.proxmox_api_token_id
   pm_api_token_secret = var.proxmox_api_token_secret
   pm_tls_insecure     = true
+  pm_timeout          = 1200
 }
